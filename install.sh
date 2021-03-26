942501701a613a20d793003ab0cf081f90ec91ab
Dustin Loring <dustinwloring1988@gmail.com>
2021-03-25 21:39:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python-de
 pip3 install -r requirments.txt
 pip3 install flask 
 apt update
-apt upgrade -y
 doker-compose up
 docker volume create portainer_data
-docker run -d -p 8011:8000 -p 9011:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d -p 8011:8000 -p 9011:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce --templates https://raw.githubusercontent.com/dustinwloring1988/templates/master/pentest
 cd CTFd/plugins
 git clone https://github.com/offsecginger/CTFd-Docker-Challenges.git
 cd CTFd-Docker-Challenges
 cp docker-challange ..
 rm -rf CTFd-Docker-Challenges
 # Completed Install Yay _ ALSO MAKE SURE TO RUN AS ROOT OR SUDO THIS
+# curl https://raw.githubusercontent.com/dustinwloring1988/CTFd-install/main/install.sh | sudo sh
